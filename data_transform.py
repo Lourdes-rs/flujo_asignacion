4b3a0e295de5db30607c4d10b953c4d988e5afef
Lourdes <lourdes.rs94@gmail.com>
2024-07-09 22:27:57 -0500

    Segunda entrega entrega de cambios para agregar 2 nuevas reglas

--- data_transform.py
+++ data_transform.py
@@ -7,8 +7,14 @@ try:
     # Leer el archivo CSV
     data = pd.read_csv(archivo_csv)
     
-    # Ordenar los datos por nombre
+    # Regla 1: Ordenar los datos por nombre
     data_ordenada = data.sort_values(by='CLIENTE')
+
+    # Regla 2: Ordenar los datos por IDCLIENTE
+    data_ordenada = data.sort_values(by='IDCLIENTE')
+    
+    # Regla 3: Ordenar los datos por departamento
+    data_ordenada = data.sort_values(by='DEPARTAMENTO')
     
     # Exportar a Excel
     archivo_excel = 'clientes_ordenados.xlsx'
